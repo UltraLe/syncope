c7f8af4a137fd2c38c30bc09c54bcf8d0e703d89
ezio <iixelite@gmail.com>
2020-11-29 18:32:16 +0100

    Configuring syncope building env.

--- common/am/lib/src/test/java/org/apache/syncope/common/lib/PitTest.java
+++ common/am/lib/src/test/java/org/apache/syncope/common/lib/PitTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 
 public class PitTest {
 
-
+    //LOCALMENTE, solo un modulo
     //mvn -pl .,common/am/lib -U -T 1C test -Dtest=org.apache.syncope.common.lib.*Test -Dinvoker.streamLogs=true -Dmodernizer.skip=true -Dianal.skip=true -Drat.skip=true -Dcheckstyle.skip=true -Dsass.skip=true -DfailIfNoTests=false
+
+    //ONLINE
+    //
 
     @Test
     public void simpleTest(){
